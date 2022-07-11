e78677cc67367b126e7a4493c19b772537274e16
Duonghieu97st <Duonghieu97st@gmail.com>
2022-07-11 17:02:08 +0700

    update html js

---
+++ index.js
@@ -0,0 +1,8 @@
+function a() {
+    let a =  document.getElementById('input()').value
+    if (a === 'ECMAScript') {
+        alert('Right!');
+    }else {
+        alert("You don't know? ECMAScript!");
+    }
+}
